760e17c5d3a54465110c7f2c1251ee8efd818934
DESKTOP-50P9098\Michael Hezaltin <hezaltin@gmail.com>
2019-11-04 08:41:08 +0530

    added-responsive-side-drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,12 @@ import React from "react";
 import Aux from "../../hoc/Auxilary";
 import classes from "./Layout.css";
 import Toolbar from '../Navigation/Toolbar/Toolbar'
-
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 const layout = props => (
   <Aux>
     {/* <div className={classes.Content}>Toolbar, sideDrawer,Backdrop</div> */}
     <Toolbar></Toolbar>
+    <SideDrawer></SideDrawer>
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
