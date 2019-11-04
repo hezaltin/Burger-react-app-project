e6aeb8e7dd7d241986cfffeb29037b20e3293261
DESKTOP-50P9098\Michael Hezaltin <hezaltin@gmail.com>
2019-11-04 09:00:35 +0530

    user-commits

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ import classes from "./Layout.css";
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-// Added the changes
+// Added the changes mm
 const layout = props => (
   <Aux>
     {/* <div className={classes.Content}>Toolbar, sideDrawer,Backdrop</div> */}
