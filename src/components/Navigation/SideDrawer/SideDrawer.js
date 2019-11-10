fa6458e2fa7479adf07599650dcd53d65ed65efa
DESKTOP-50P9098\Michael Hezaltin <hezaltin@gmail.com>
2019-11-10 12:08:25 +0530

    core-concepts-of-react-with-the-components

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,11 +2,20 @@ import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./SideDrawer.css";
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxilary/Auxilary';
 
 const sideDrawer = props => {
   //....
+  let attachedClasses = [classes.SideDrawer,classes.Close];
+
+  if(props.open){
+    attachedClasses = [classes.SideDrawer,classes.Open];
+  }
   return (
-    <div className={classes.SideDrawer}>
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed}></Backdrop>
+      <div className={attachedClasses.join(' ')}>
       <div className={classes.Logo}>
         <Logo></Logo>
       </div>
@@ -15,6 +24,8 @@ const sideDrawer = props => {
         <NavigationItems></NavigationItems>
       </nav>
     </div>
+    </Aux>
+    
   );
 };
 
